c44e663cc74d8d9ae06a731628ae07078b7732c7
Jatin <jatin6972@gmail.com>
2019-04-01 12:56:13 +0530

    created index page

---
+++ app.js
@@ -0,0 +1,63 @@
+//requiring all modules
+const axios = require("axios");
+const express = require("express");
+// const expressip = require('express-ip')
+const path = require("path");
+const handlebars = require("express-handlebars");
+
+
+//port and starting express
+const app = express();
+const PORT = process.env.PORT || 5555;
+
+const exphbs = require('express-handlebars')
+
+//templating language defining
+app.set("view engine", "hbs");
+
+// app.use(expressip().getIpInfoMiddleware);
+app.use(express.static(path.join(__dirname, "assets")));
+
+// get request to initial url
+// will open index.hbs file
+app.get("/", (req, res) => {
+  res.render('index', {
+    title: 'Job Search'
+  })
+});
+
+app.get("/search", (req, res) => {
+  //query to search on api
+  queries = req.query;
+
+  //api to find job
+  let url = `https://indreed.herokuapp.com/api/jobs`;
+
+  if (queries) {
+    axios
+      .get(url, {
+        params: queries
+      })
+      .then(response => {
+        console.log("inside then axios");
+        res.render("search", {
+          title: "Job Search",
+          jobs: response.data
+        });
+
+        // res.send(response.data)
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  } else {
+    res.render("search", {
+      title: "Job Search"
+    });
+  }
+});
+
+//server running on port
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+  var typed = new Typed('#typed', {
+     stringsElement: '#typed-strings',
+     typeSpeed: 100,
+     startDelay: 0,
+     backSpeed: 20,
+     loop: true
+  });
