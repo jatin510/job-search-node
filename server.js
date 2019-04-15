0237887018b8b00df327c8d561e9ea7e69342d12
jatin510 <jatin6972@gmail.com>
2019-04-15 22:21:48 +0530

    alternate to bad api request

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const axios = require("axios");
 const express = require("express");
 // const expressip = require('express-ip')
 const path = require("path");
-const handlebars = require("express-handlebars");
-
+// const handlebars = require("express-handlebars");
 
 //port and starting express
 const app = express();
 const PORT = process.env.PORT || 5555;
 
-const exphbs = require('express-handlebars')
+const exphbs = require("express-handlebars");
 
 //templating language defining
 app.set("view engine", "hbs");
@@ -18,21 +17,21 @@ app.set("view engine", "hbs");
 // app.use(expressip().getIpInfoMiddleware);
 app.use(express.static(path.join(__dirname, "assets")));
 
-var users = []
-
+var users = [];
 
 // get request to initial url
 // will open index.hbs file
 app.get("/", (req, res) => {
-  res.render('index', {
-    title: 'Job Search'
-  })
+  res.render("index", {
+    title: "Job Search"
+  });
 });
 
 app.get("/search", (req, res) => {
   //query to search on api
   queries = req.query;
-
+  queries['max'] = 15
+  console.log(queries)
   //api to find job
   let url = `https://indreed.herokuapp.com/api/jobs`;
 
@@ -42,13 +41,12 @@ app.get("/search", (req, res) => {
         params: queries
       })
       .then(response => {
-        console.log("inside then axios");
+        // console.log("inside then axios");
         res.render("search", {
           title: "Job Search",
           jobs: response.data
         });
-
-        // res.send(response.data)
+        // res.send(response.data);
       })
       .catch(error => {
         console.log(error);
@@ -61,29 +59,27 @@ app.get("/search", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.render('about', {
-    title: 'Job Search'
-  })
+  res.render("about", {
+    title: "Job Search"
+  });
 });
 
 app.get("/contact", (req, res) => {
-  res.render('contact', {
-    title: 'Job Search'
-  })
+  res.render("contact", {
+    title: "Job Search"
+  });
 });
 
-
-app.post('/feedback', (req, res) => {
+app.post("/feedback", (req, res) => {
   users.push({
     name: req.body.name,
     email: req.body.email,
     country: req.body.country,
     message: req.body.message
+  });
 
-  })
-
-  res.send()
-})
+  res.send();
+});
 
 //server running on port
 app.listen(PORT, () => {
